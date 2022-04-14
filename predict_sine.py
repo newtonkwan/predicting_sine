9a9caebeb4d9d8c58d8551bcc9ea2998dce852a3
Newton Kwan <nk@2398.com>
2022-04-14 13:43:27 +0100

    Expand variable names for readability

--- predict_sine.py
+++ predict_sine.py
@@ -1,4 +1,4 @@
-# Use JAX to predict the sine function. 
+# Predict the sine function using pure JAX. 
 # Date created: April 13, 2022
 # Last updated: April 14, 2022 
 
@@ -9,81 +9,89 @@ import matplotlib.pyplot as plt
 import time
 import hydra
 
-def generate_sin(N, key):
+def generate_sin(num_examples, key):
     '''Generates samples from sin function'''
-    xTr = jax.random.uniform(key, shape=(N, 1), minval=0, maxval=2*np.pi)
-    yTr = jnp.sin(xTr)
-    xTe = jax.random.uniform(key, shape=(N//10, 1), minval=0, maxval=2*np.pi)
-    yTe = jnp.sin(xTe)
-    return xTr, yTr, xTe, yTe
+    x_train = jax.random.uniform(key, shape=(num_examples, 1), minval=0, maxval=2*np.pi)
+    y_train = jnp.sin(x_train)
+    x_test = jax.random.uniform(key, shape=(num_examples//10, 1), minval=0, maxval=2*np.pi)
+    y_test = jnp.sin(x_test)
+    return x_train, y_train, x_test, y_test
 
-def init_MLP(layer_widths, init_key, scale=1e-2): 
+def init_MLP(layer_sizes, init_key, scale=1e-2): 
     '''Initialize MLP'''
     params = [] 
-    keys = jax.random.split(init_key, len(layer_widths)-1)
-    for in_width, out_width, key in zip(layer_widths[:-1], layer_widths[1:], keys): 
+    keys = jax.random.split(init_key, len(layer_sizes)-1)
+    for input_size, output_size, key in zip(layer_sizes[:-1], layer_sizes[1:], keys): 
         weight_key, bias_key = jax.random.split(key)
         params.append([
             # scaling makes for a better initialization
-            scale * jax.random.normal(weight_key, shape=(out_width, in_width)), 
-            scale * jax.random.normal(bias_key, shape=(out_width,))
+            scale * jax.random.normal(weight_key, shape=(output_size, input_size)), 
+            scale * jax.random.normal(bias_key, shape=(output_size,))
             ]
         )
     return params
 
-def predict_MLP(params, x):
+def predict_MLP(params, input_data):
     '''Predict using the MLP'''
     hidden_layers = params[:-1]
-    activation = x
+    activation = input_data
     # all layers minus the last layer
-    for w,b in hidden_layers: 
-        activation = jax.nn.relu(jnp.dot(w, activation) + b)
+    for weights, bias in hidden_layers: 
+        activation = jax.nn.relu(jnp.dot(weights, activation) + bias)
     
     # last layer
-    final_w, final_b = params[-1]
-    prediction = jnp.dot(final_w, activation) + final_b
+    final_weights, final_bias = params[-1]
+    prediction = jnp.dot(final_weights, activation) + final_bias
     return prediction  
 
-def loss(params, x, y):
+def loss(params, x_inputs, y_target_outputs):
     '''Loss function'''
     batched_predict_MLP = jax.vmap(predict_MLP, in_axes=(None, 0))
-    return jnp.mean((batched_predict_MLP(params, x) - y) **2) # MSE loss 
+    return jnp.mean((batched_predict_MLP(params, x_inputs) - y_target_outputs) **2) # MSE loss 
 
 @jax.jit
-def update(params, x, y, lr):
+def update(params, x_train, y_train, lr):
     '''Calculate gradients and update parameters'''
-    grads = jax.grad(loss)(params, x, y)
-    return jax.tree_multimap(lambda p, g: p - lr*g, params, grads) #SGD 
+    grads = jax.grad(loss)(params, x_train, y_train)
+    return jax.tree_multimap(lambda params_i, grads_i: params_i - lr*grads_i, params, grads) #SGD 
 
-def training_loop(params, x, y, x_test, y_test, lr, num_epochs, batch_size):
+def training_loop(params, x_train, y_train, x_test, y_test, lr, num_epochs, batch_size):
     '''Training loop'''
+    num_examples = len(x_train) 
     for epoch in range(num_epochs):
         start_time = time.time()
-        for i in range(0, len(x), batch_size):
-            if i + batch_size < len(x):
-                params = update(params, x[i:i+batch_size], y[i:i+batch_size], lr)
+        for batch_start in range(0, num_examples, batch_size):
+            if batch_start + batch_size < num_examples:
+                params = update(
+                        params, 
+                        x_train[batch_start:batch_start+batch_size], 
+                        y_train[batch_start:batch_start+batch_size], 
+                        lr)
             else: 
                 # last batch is smaller if batch size not divisable
-                params = update(params, x[i:len(x)], y[i:len(x)], lr)
+                params = update(params, x_train[batch_start:num_examples], x_train[batch_start:num_examples], lr)
         epoch_time = time.time() - start_time
-        train_acc = loss(params, x, y)
-        test_acc = loss(params,x_test, y_test)
+        train_mse = loss(params, x_train, y_train)
+        test_mse = loss(params,x_test, y_test)
         print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
-        print("Training set accuracy {}".format(train_acc))
-        print("Test set accuracy {}".format(test_acc))
+        print("Training set accuracy {}".format(train_mse))
+        print("Test set accuracy {}".format(test_mse))
     return params
 
 # TODO: write a save function 
+def save_model(params):
+    '''Save model parameters'''
+    return
 
 @hydra.main(config_path="conf", config_name="config")
 def main(args): 
     key = jax.random.PRNGKey(args.seed)
-    xTr, yTr, xTe, yTe = generate_sin(args.num_examples, key)
+    x_train, y_train, x_test, y_test = generate_sin(args.num_examples, key)
     MLP_params = init_MLP([1, 128, 128, 1], key)
     batched_predict_MLP = jax.vmap(predict_MLP, in_axes=(None, 0))
-    MLP_params = training_loop(MLP_params, xTr, yTr, xTe, yTe, args.lr, args.epochs, args.batch_size)
-    plt.scatter(xTr, yTr, s=3, label="Training data")
-    plt.scatter(xTe, batched_predict_MLP(MLP_params, xTe), s=3, label = "Model Prediction")
+    MLP_params = training_loop(MLP_params, x_train, y_train, x_test, y_test, args.learning_rate, args.epochs, args.batch_size)
+    plt.scatter(x_train, y_train, s=3, label="Training data")
+    plt.scatter(x_test, batched_predict_MLP(MLP_params, x_test), s=3, label = "Model Prediction")
     plt.legend()
     plt.show()
 
