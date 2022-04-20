Add cleaner batch functionality, fix bug with training loss calculation, move step function out of training

predict_sine_with_haiku.py
@@ -15,27 +15,24 @@ import pickle
 from pathlib import Path
 from typing import Union
 
-# TODO: Convert functions into a class
-class SimpleSine():
-    '''Simple MLP to predict the sine function'''
-    def __init__(): 
-        pass 
 
 def forward(batch: jnp.ndarray) -> jnp.ndarray:
     '''Standard MLP network'''
     mlp = hk.nets.MLP(output_sizes=[128, 128, 1], w_init = hk.initializers.RandomNormal(stddev=1e-2), b_init = hk.initializers.RandomNormal(stddev=1e-2), name="standard_mlp")
     return mlp(batch)
 
-def generate_sin(num_examples, key):
+def generate_sin(num_examples, batch_size, key):
     '''Generates samples from sin function'''
-    x_train = jax.random.uniform(key, shape=(num_examples, 1), minval=0, maxval=2*np.pi)
+    key, init_key = jax.random.split(key)
+    x_train = jax.random.uniform(init_key, shape=(num_examples, batch_size, 1), minval=0, maxval=2*np.pi)
     y_train = jnp.sin(x_train)
-    x_test = jax.random.uniform(key, shape=(num_examples//10, 1), minval=0, maxval=2*np.pi)
+    x_test = jax.random.uniform(key, shape=(num_examples//10, batch_size, 1), minval=0, maxval=2*np.pi)
     y_test = jnp.sin(x_test)
     return x_train, y_train, x_test, y_test
 
 # Save and load functions from https://github.com/google/jax/issues/2116
 def save(data, path: Union[str, Path], overwrite: bool = False):
+    '''Save function'''
     suffix = '.pickle'
     path = Path(path)
     if path.suffix != suffix:
@@ -50,6 +47,7 @@ def save(data, path: Union[str, Path], overwrite: bool = False):
         pickle.dump(data, file)
 
 def load(path: Union[str, Path]):
+    '''Load function'''
     suffix = '.pickle'
     path = Path(path)
     if not path.is_file():
@@ -67,49 +65,50 @@ def main(args):
     optimizer = optax.adam(learning_rate = args.learning_rate)
 
     def loss(params, batch: jnp.ndarray, labels: jnp.ndarray):
+        '''Loss function'''
         y_hat = apply_net(params, batch)
-        loss_value = optax.l2_loss(y_hat, labels).sum(axis=-1) 
-        return loss_value.mean() # MSE 
+        loss_value = optax.l2_loss(y_hat, labels).mean() # MSE
+        return loss_value
 
+    @jax.jit
+    def step(params, opt_state, batch, labels):
+        '''Calculate gradients and update parameters'''
+        loss_value, grads = jax.value_and_grad(loss)(params, batch, labels)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss_value
+    
     def training_loop(params, optimizer, x_train, y_train, x_test, y_test, lr, num_epochs, batch_size):
         '''Training loop'''
         opt_state = optimizer.init(params)
-        train_losses = []
-        test_losses = []
-
-        @jax.jit
-        def step(params, opt_state, x_train, y_train):
-            '''Calculate gradients and update parameters'''
-            loss_value, grads = jax.value_and_grad(loss)(params, x_train, y_train)
-            updates, opt_state = optimizer.update(grads, opt_state, params)
-            params = optax.apply_updates(params, updates)
-            return params, opt_state, loss_value
+        train_losses, test_losses = [], []
 
         num_examples = len(x_train) 
         for epoch in range(num_epochs):
+            epoch_sum_of_train_loss = 0 
             start_time = time.time()
-            for batch_start in range(0, num_examples, batch_size):
-                if batch_start + batch_size < num_examples:
-                    params, opt_state, loss_value = step(
-                        params, 
-                        opt_state,
-                        x_train[batch_start:batch_start+batch_size], 
-                        y_train[batch_start:batch_start+batch_size])
-                else: 
-                    # last batch is smaller if batch size not divisable
-                    params, opt_state, loss_value = step(
-                        params, 
-                        opt_state,
-                        x_train[batch_start:num_examples], 
-                        y_train[batch_start:num_examples])
+            for batch_i in range(0, num_examples):    
+                params, opt_state, step_train_loss = step(
+                    params, 
+                    opt_state,
+                    x_train[batch_i], 
+                    y_train[batch_i])
+                epoch_sum_of_train_loss += step_train_loss
             epoch_time = time.time() - start_time
-            train_loss = loss_value
-            test_loss = loss(params,x_test, y_test)
+
+            # calculate train loss 
+            epoch_train_loss = epoch_sum_of_train_loss/len(x_train) # avg train loss per batch per epoch
+            train_losses.append(epoch_train_loss)
+
+            # calculate test loss 
+            flattened_x_test = x_test.reshape(-1, 1)
+            flattened_y_test = y_test.reshape(-1, 1)
+            epoch_test_loss = loss(params, flattened_x_test, flattened_y_test)
+            test_losses.append(epoch_test_loss)
+
             print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
-            print("Training loss {}".format(train_loss))
-            print("Test loss {}".format(test_loss))
-            train_losses.append(train_loss)
-            test_losses.append(test_loss)
+            print("Training loss {}".format(epoch_train_loss))
+            print("Test loss {}".format(epoch_test_loss))
         return params, train_losses, test_losses
 
     def plot(network_params, x_train, y_train, x_test, train_losses, test_losses, epochs):
@@ -127,7 +126,7 @@ def main(args):
         ax2.set_title("Training and Test Losses")
         path = os.path.dirname(os.path.abspath(__file__))
         print("Saving plot ... ")
-        plt.savefig(path+'/plots/haiku_plots.png', bbox_inches='tight', dpi=800)
+        plt.savefig(path+'/plots/haiku_plots.png', bbox_inches='tight', dpi=400)
         print(f"Done. Path to plot: {path+'/plots/haiku_plots.png'}")
         plt.show()
 
@@ -136,13 +135,14 @@ def main(args):
     key, init_key = jax.random.split(key)
 
     # Generate training data
-    x_train, y_train, x_test, y_test = generate_sin(args.num_examples, init_key)
+    x_train, y_train, x_test, y_test = generate_sin(args.num_batches, args.batch_size, init_key)
 
     # Initialize the network; note we draw an input to get shapes
     sample_input = jax.random.uniform(key, shape=(1, 1), minval=0, maxval=2*np.pi)
-    layer_sizes = [args.layers.layer1_size, 
-                args.layers.layer2_size, 
-                args.layers.output_size]
+    # Can't include layer sizes unless i want to pass in layer sizes every call of apply_net()
+    # layer_sizes = [args.layers.layer1_size, 
+    #             args.layers.layer2_size, 
+    #             args.layers.output_size]
     network_params = init_net(key, sample_input)
 
     # Train model 